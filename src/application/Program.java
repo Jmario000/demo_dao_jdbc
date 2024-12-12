7c6e31acffb04e729ef368ae7320c7c8079c63d9
José Mário Neto <jose.mario55@proton.me>
2024-12-12 10:15:36 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,20 @@
 package application;
 
+import java.time.LocalDate;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+
+		LocalDate date = LocalDate.of(2000, 5, 25);
+
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", date, 3000.0, obj);
+		System.out.println(seller);
 	}
 
 }
